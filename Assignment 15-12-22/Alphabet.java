97dd0b6149e9de117445cbb22785ddc98355cc43
Shawin Pradhan <62380278+ShawinPradhan@users.noreply.github.com>
2022-12-16 11:15:54 +0530

    Update Alphabet.java

--- Assignment 15-12-22/Alphabet.java
+++ Assignment 15-12-22/Alphabet.java
@@ -5,7 +5,7 @@ package basic;
 
 import java.util.Scanner;
 
-public class Alphabet {
+public class Alphabet {   
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
